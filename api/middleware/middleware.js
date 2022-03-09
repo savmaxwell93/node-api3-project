6963c3ee5072373011cf94f52d9448a1f5724cf1
Savannah <savmaxwell@gmail.com>
2022-03-09 12:57:40 -0700

    wip

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -9,15 +9,36 @@ function logger(req, res, next) {
 }
 
 function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+  User.getById(req.params.id)
+    .then(user => {
+      if (!user) {
+        res.status(404).json({ message: `user not found`});
+      } else {
+        req.user = user;
+        next();
+      }
+    })
+    .catch(next);
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  const { name } = req.body;
+  if (!name || !name.trim()) {
+    res.status(400).json({ message: "missing required name field"});
+  } else {
+    req.name = name.trim();
+    next();
+  }
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  const { text } = req.body;
+  if (!text || !text.trim()) {
+    res.status(400).json({ message: "missing required text field"});
+  } else {
+    req.text = text.trim();
+    next();
+  }
 }
 
 module.exports = {
