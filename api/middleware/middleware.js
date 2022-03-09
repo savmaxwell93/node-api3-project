51a2861eabbe237824c1cbb116c69f610cee0d81
Savannah <savmaxwell@gmail.com>
2022-03-09 12:43:49 -0700

    wip

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,7 +1,11 @@
 const User = require('../users/users-model');
 
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  const method = req.method;
+  const url = req.originalUrl;
+  const timestamp = new Date().toLocaleString();
+  console.log(`Logger: ${timestamp} ${method} to ${url}`);
+  next();
 }
 
 function validateUserId(req, res, next) {
